b4d69c5888597b704071be7345d956c548b64cf9
Eduardo <eduardo42bieger@gmail.com>
2024-02-11 18:42:16 -0300

    configured new registers

--- src/app.js
+++ src/app.js
@@ -2,8 +2,10 @@ const fastify = require("fastify")({
   logger: true
 });
 
-fastify.register(require("./routes/index"));
 fastify.register(require("./plugins/dbConnector"));
+fastify.register(require("@fastify/postgres"));
+fastify.register(require("./routes/index"));
+fastify.register(require("./routes/testRoutes"));
 
 fastify.listen({ port: 3000 }, (err) => {
   if (err) {
