4eb43437e38e13c77fdb5f99a391d848ecb1e5d0
Eduardo <eduardo42bieger@gmail.com>
2024-02-11 13:48:06 -0300

    created file with environment variables configuration

---
+++ config/env.js
@@ -0,0 +1,6 @@
+module.exports = {
+  postgresqlUrl: process.env.POSTGRESQL_URL,
+  postgresqlUsername: process.env.POSTGRESQL_USERNAME,
+  postgresqlPassword: process.env.POSTGRESQL_PASSWORD,
+  postgresqlDatabase: process.env.POSTGRESQL_DATABASE
+}
