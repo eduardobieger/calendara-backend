f8d223ca214116b3b3d296847544316c1b561d2a
Eduardo <eduardo42bieger@gmail.com>
2024-02-24 19:44:28 -0300

    added primary key to uuid

--- db.sql
+++ db.sql
@@ -1,7 +1,7 @@
 CREATE DATABASE calendara_database;
 
 CREATE TABLE users_table(
-  user_id uuid DEFAULT gen_random_uuid(),
+  user_id uuid PRIMARY KEY DEFAULT gen_random_uuid(),
   username VARCHAR(30) UNIQUE NOT NULL,
   password VARCHAR(30) NOT NULL,
   email TEXT UNIQUE NOT NULL,
@@ -9,7 +9,7 @@ CREATE TABLE users_table(
 );
 
 CREATE TABLE appointments_table(
-  appointment_id uuid DEFAULT gen_random_uuid(),
+  appointment_id uuid PRIMARY KEY DEFAULT gen_random_uuid(),
   title TEXT NOT NULL,
   start_date TIMESTAMP NOT NULL,
   end_date TIMESTAMP NOT NULL
