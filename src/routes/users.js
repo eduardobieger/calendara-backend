ad16f3731217131c2585cf95d9d02d25f17f6191
Eduardo <eduardo42bieger@gmail.com>
2024-02-24 18:04:17 -0300

    create routes to manage users

---
+++ src/routes/users.js
@@ -0,0 +1,13 @@
+const usersPostController = require("../controllers/usersPostController");
+
+async function users(fastify, options) {
+  fastify.get("/users", async (request, reply) => {
+    return { users: [] };
+  });
+
+  fastify.post("/users", async (request, reply) => {
+    return reply.code(201).send({ success: true });
+  });
+}
+
+module.exports = users;
