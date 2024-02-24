df24c2cf5ee98fb0ec1d194cc26f697bfb862981
Eduardo <eduardo42bieger@gmail.com>
2024-02-24 18:12:29 -0300

    sql file with tables creation details

---
+++ db.sql
@@ -0,0 +1,16 @@
+CREATE DATABASE calendara_database;
+
+CREATE TABLE users_table(
+  user_id uuid DEFAULT gen_random_uuid(),
+  username VARCHAR(30) UNIQUE NOT NULL,
+  password VARCHAR(30) NOT NULL,
+  email TEXT UNIQUE NOT NULL,
+  display_name TEXT NOT NULL
+);
+
+CREATE TABLE appointments_table(
+  appointment_id uuid DEFAULT gen_random_uuid(),
+  title TEXT NOT NULL,
+  start_date TIMESTAMP NOT NULL,
+  end_date TIMESTAMP NOT NULL
+);
