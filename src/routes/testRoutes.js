070214fb539c9c18ba0c1a50a1b564aef35bbe96
Eduardo <eduardo42bieger@gmail.com>
2024-02-24 17:37:50 -0300

    removed unused files

--- src/routes/testRoutes.js
+++
@@ -1,6 +0,0 @@
-module.exports = async (fastify, options) => {
-  fastify.get("/test", async (request, reply) => {
-    const { rows } = await fastify.pg.query("SELECT * FROM test_table");
-    reply.send(rows);
-  })
-}
