0aaaefbdfa7c8ecc21d3a6b4644efa1233353ef4
Eduardo <eduardo42bieger@gmail.com>
2024-02-25 00:03:43 -0300

    added salt rounds environment variable

--- config/env.js
+++ config/env.js
@@ -4,10 +4,12 @@ const postgresqlHost = process.env.POSTGRESQL_HOST;
 const postgresqlUsername = process.env.POSTGRESQL_USERNAME;
 const postgresqlPassword = process.env.POSTGRESQL_PASSWORD;
 const postgresqlDatabase = process.env.POSTGRESQL_DATABASE;
+const bcryptSaltRounds = process.env.BCRYPT_SALT_ROUNDS;
 
 module.exports = {
   postgresqlHost,
   postgresqlUsername,
   postgresqlPassword,
-  postgresqlDatabase
-}
+  postgresqlDatabase,
+  bcryptSaltRounds,
+};
