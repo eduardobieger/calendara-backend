ab17c3911c19439499d7d0fa58e310ba20e7118c
Eduardo <eduardo42bieger@gmail.com>
2024-02-25 00:30:10 -0300

    returning error message in case of error

--- src/controllers/usersPostController.js
+++ src/controllers/usersPostController.js
@@ -16,7 +16,7 @@ async function usersPostController(fastify, request, reply) {
     return reply.code(201).send({ success: true });
   } catch (err) {
     fastify.log.error(err);
-    return reply.code(500).send({ message: err });
+    return reply.code(500).send({ message: err.message });
   }
 }
 
