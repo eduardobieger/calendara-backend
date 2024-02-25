961c94cffd4c796f5603ed8d30feab1e888c2204
Eduardo <eduardo42bieger@gmail.com>
2024-02-25 06:46:19 -0300

    changed return

--- src/controllers/usersGetAllController.js
+++ src/controllers/usersGetAllController.js
@@ -2,11 +2,11 @@ const env = require("../../config/env");
 
 async function usersGetAllController(fastify, request, reply) {
   try {
-    fastify.pg.query(
+    const result = await fastify.pg.query(
       "SELECT user_id, username, email, display_name FROM users_table"
     );
 
-    return reply.code(200).send(err | result);
+    return reply.code(200).send({ result });
   } catch (err) {
     fastify.log.error(err);
     return reply.code(500).send({ message: err.message });
