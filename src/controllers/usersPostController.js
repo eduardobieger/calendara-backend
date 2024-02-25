20ac0b2fb96939c8eba5ec0fefcae89e8e8b7780
Eduardo <eduardo42bieger@gmail.com>
2024-02-25 00:04:22 -0300

    using bcrypt to hash password and insert it in database

--- src/controllers/usersPostController.js
+++ src/controllers/usersPostController.js
@@ -1,12 +1,15 @@
 const env = require("../../config/env");
+const bcrypt = require("bcrypt");
 
 async function usersPostController(fastify, request, reply) {
   const { username, email, password, displayName } = request.body;
 
   try {
+    const hashedPassword = await bcrypt.hash(password, env.bcryptSaltRounds);
+
     fastify.pg.query(
-      "INSERT INTO users_table(username, password, email, display_name) VALUES($1, crypt($2, gen_salt(bf)), $3, $4)",
-      [username, password, email, displayName]
+      "INSERT INTO users_table(username, password, email, display_name) VALUES($1, $2, $3, $4)",
+      [username, hashedPassword, email, displayName]
     );
 
     return reply.code(201).send({ success: true });
