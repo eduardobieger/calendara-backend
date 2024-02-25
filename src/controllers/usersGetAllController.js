b4ab5a8582baaf2a83c85ff7e6c450e73c9c9874
Eduardo <eduardo42bieger@gmail.com>
2024-02-25 06:41:23 -0300

    removed internal function to return

--- src/controllers/usersGetAllController.js
+++ src/controllers/usersGetAllController.js
@@ -3,11 +3,10 @@ const env = require("../../config/env");
 async function usersGetAllController(fastify, request, reply) {
   try {
     fastify.pg.query(
-      "SELECT user_id, username, email, display_name FROM users_table",
-      function onResult(err, result) {
-        return reply.code(200).send(err | result);
-      }
+      "SELECT user_id, username, email, display_name FROM users_table"
     );
+
+    return reply.code(200).send(err | result);
   } catch (err) {
     fastify.log.error(err);
     return reply.code(500).send({ message: err.message });
